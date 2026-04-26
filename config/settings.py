e21731afd5affbbeb910cdfec641038252b6ff73
Carlos <carlos.arca.barca.1970@gmail.com>
2026-04-26 23:00:00 +0200

    Add CSRF trusted origins

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,11 @@ ALLOWED_HOSTS = os.environ.get(
     "127.0.0.1,localhost"
 ).split(",")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    "CSRF_TRUSTED_ORIGINS",
+    "http://127.0.0.1,http://localhost"
+).split(",")
+
 
 # =========================
 # APPS
